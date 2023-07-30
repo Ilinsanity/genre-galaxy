9667d723326a525dc8b84fb16bff2db12f59f87e
Issac Lin <issaclin2002@gmail.com>
2023-07-29 23:05:17 -0400

    Starting project - getting spotify auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const clientId = "4e7f52bb41af4f24a16bae8895f81f56";
+const redirectUri = "http://localhost:3000/callback/";
+
+function LogIn() {
+  let codeVerifier = generateRandomString(128);
+  generateCodeChallenge(codeVerifier).then((codeChallenge) => {
+    let state = generateRandomString(16);
+    let scope = "user-read-private user-read-email user-top-read";
+
+    localStorage.setItem("code_verifier", codeVerifier);
+
+    let args = new URLSearchParams({
+      response_type: "code",
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state,
+      code_challenge_method: "S256",
+      code_challenge: codeChallenge,
+    });
+
+    window.location = "https://accounts.spotify.com/authorize?" + args;
+  });
+
+  function generateRandomString(length) {
+    let text = "";
+    let possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+    }
+
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+    return base64encode(digest);
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get("code");
+
+  // let codeVerifier = localStorage.getItem("code_verifier");
+
+  let body = new URLSearchParams({
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: redirectUri,
+    client_id: clientId,
+    code_verifier: codeVerifier,
+  });
+
+  const response = fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: body,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("HTTP status " + response.status);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem("access_token", data.access_token);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Genre Galaxy</h1>
+      <button onClick={LogIn}>Log In</button>
     </div>
   );
 }
